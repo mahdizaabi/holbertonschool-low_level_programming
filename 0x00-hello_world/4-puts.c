04657adbcb460ae1ca6c2dc96beb0eceffd96bb5
mahdizaabi <mahdi.zaabi@gmail.com>
2020-02-05 19:51:16 +0000

    adding file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,2 @@
-#include <stdio.h>
-int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-}
+#!/bin/bash
+puts ("\"Programming is like building a multilingual puzzle")
