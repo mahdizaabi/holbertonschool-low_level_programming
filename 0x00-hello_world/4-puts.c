da419b1fb7c44016ee6f5c6ae1bac8be79a056c3
mahdizaabi <mahdi.zaabi@gmail.com>
2020-02-05 15:45:56 +0000

    adding file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - Printing something on the screen with PUTS
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
