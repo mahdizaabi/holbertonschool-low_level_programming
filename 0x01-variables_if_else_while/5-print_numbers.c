4d77af8f86687766e181777138c500482ef2acef
mahdizaabi <mahdi.zaabi@gmail.com>
2020-02-06 22:38:11 +0000

    adding

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Description:printing digit number
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		printf("%d", x);
+	}
+	putchar('\n');
+	return (0);
+}
