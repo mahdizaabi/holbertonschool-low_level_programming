0b53259cd8239c698d0402f05e93ea0b84b82013
mahdizaabi <mahdi.zaabi@gmail.com>
2020-02-06 22:40:45 +0000

    adding file

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Description:printinggg single digits numbers of base 10 starting from 01
+ * Return: 0
+ *sdssssssdssdsdsdsds*/
+
+int main(void)
+{
+	int x;
+
+	for (x = 48; x <= 57; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+
+	return (0);
+}
