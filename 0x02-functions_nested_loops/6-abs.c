7ffd3c4ca3679d6af3e22f79f94f53e25bcdfba6
mahdizaabi <mahdi.zaabi@gmail.com>
2020-02-11 18:24:17 +0000

    adding file

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * int _abs(int) - Computing the absolut value of an given number.
+ * @n: The variable Input
+ *
+ * Return: 1 if n positive,
+ *         0 if n equal to zero,
+ *         -1 if n negative.
+ */
+int _abs(int)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
