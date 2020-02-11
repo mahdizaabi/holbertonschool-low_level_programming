f99ee80773221cb960c0447ba35986bfb20ef8e6
mahdizaabi <mahdi.zaabi@gmail.com>
2020-02-11 18:46:58 +0000

    adding

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -4,14 +4,16 @@
  * _abs - Computing the absolut value of an given number.
  * @int: The variable Input
  *
- * Return: 1 if n positive,
- *         0 if n equal to zero,
- *         -1 if n negative.
+ * Return: n if n positive,
+ *         -n if n negative,
+ *
  */
 int _abs(int)
 {
 	if (n >= 0)
+	{
 		return (n);
+	}
 	else
-		return (-n);
+		return ((-1) * n);
 }
