7587e0873270c5246ab20b622fa1260bef049044
mahdizaabi <mahdi.zaabi@gmail.com>
2020-02-11 20:26:10 +0000

    addingfile

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * add - Addition of two integers.
+ * @num1: integer1.
+ * @num2: integer2.
+ *
+ * Return: addition result.
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
