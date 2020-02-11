e7a2416354061b176f711b2a5252e5c3d6a3b0bc
mahdizaabi <mahdi.zaabi@gmail.com>
2020-02-11 16:14:11 +0000

    adding file

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+* main - function to check for lowercase and uppercase character
+*@i - print alphbet
+*Return: 1 if c is lowercase,letter,uppercase, otherwise 0
+*/
+int main(void)
+{
+	int = i;
+
+	if (i >= 'a' && i <= 'z' || i >= 'A' && i <= 'Z')
+
+		return (1);
+
+	else
+
+		return (0);
+
+}
