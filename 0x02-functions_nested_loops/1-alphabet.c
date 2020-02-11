5845be8b9363e7bdaba8d39d7bc2e9e6a3df16a0
mahdizaabi <mahdi.zaabi@gmail.com>
2020-02-11 08:18:41 +0000

    adding file

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * print_alphabet fonction
+ *
+ */
+void print_alphabet(void)
+{
+	int c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
