0f13d6e1d9969126c808f2d0d2c698cc9c7cb38e
mahdizaabi <mahdi.zaabi@gmail.com>
2020-02-11 20:40:03 +0000

    adding file

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - all natural numbers below 13
+ *
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i, sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+			sum += i;
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
