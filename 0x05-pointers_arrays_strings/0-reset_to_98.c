8847a1f30e026720c493b4cc882b89534453f324
mahdizaabi <mahdi.zaabi@gmail.com>
2020-02-17 16:07:51 +0000

    adding file

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,18 @@
+/*
+ * Project : 0. 98 Battery st.
+ * Programmer: Mehdi Zaabi
+ * For Holberton School 2020
+ */
+
+#include "holberton.h"
+
+/**
+ * reset_to_98 - resets value of pointer to 98
+ * @n: pointer to reset to 98
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
