20da339af1604229528ea8151ee1baf0fb23bd1a
mahdizaabi <mahdi.zaabi@gmail.com>
2020-02-18 21:01:24 +0000

    adding file

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,14 +1,14 @@
 #include "holberton.h"
 
 /**
- * puts2 - Printing one char
- * @str: the string.
+ * puts2 - Prints one char out of two of a string.
+ * @str: The string containing characters.
  */
 void puts2(char *str)
 {
 	int i, len = 0;
 
-	while (str[i])
+	while (str[i++])
 		len++;
 
 	for (i = 0; i < len; i += 2)
