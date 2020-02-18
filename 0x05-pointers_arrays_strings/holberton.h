b3ca2602f1409634bb06a0e2e796b5bbb9f6f01f
mahdizaabi <mahdi.zaabi@gmail.com>
2020-02-18 18:17:15 +0000

    adding file

--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -1,5 +1,10 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <stdlib.h>
+
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
-int _strlen(char *s);
-void _puts(char *str);
-int _putchar(char c);
+size_t _strlen(const char *str);
+
+#endif
