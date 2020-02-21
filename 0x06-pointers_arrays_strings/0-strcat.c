fe6da831dbe36f340e6180eb390df1c4d2d96937
mahdizaabi <mahdi.zaabi@gmail.com>
2020-02-21 21:20:07 +0000

    addingfile

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,23 +1,32 @@
 #include "holberton.h"
 
 /**
- *_strcat - appends src to the dest string
- *@dest: string to append by src
- *@src: string to append to dest
- *
- *Return: address of dest
+ * _strcat - function
+ *@dest: string
+ *@src: string2
+ *Return: dest
  */
+
 char *_strcat(char *dest, char *src)
 {
-	int i, j;
+int i, j;
 
-	i = j = 0;
-	while (*(dest + i))
-		i++;
-	while ((*(dest + i) = *(src + j)))
+	i = 0;
+	while (dest[i] != '\0')
 	{
 		i++;
-		j++;
 	}
+
+		j = 0;
+	while (src[j] != '\0')
+	{
+	dest[i] = src[j];
+	i++;
+	j++;
+	}
+
+
+	dest[i] = '\0';
+
 	return (dest);
 }
