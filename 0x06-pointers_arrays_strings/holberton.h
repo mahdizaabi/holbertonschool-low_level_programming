14ac076504335dcf97158b93f73c4feff790dbc1
mahdizaabi <mahdi.zaabi@gmail.com>
2020-02-21 21:28:50 +0000

    addingfile

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -1,7 +1,11 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
-#include <stdlib.h>
-
-char *_strcat(char *dest, char *src);
-
-#endif
+int _putchar(char);
+char *_strcat(char*, char*);
+char *_strncat(char*, char*, int);
+char *_strncpy(char*, char*, int);
+int _strcmp(char*, char*);
+void reverse_array(int*, int);
+char *string_toupper(char*);
+char *cap_string(char*);
+char *leet(char*);
+char *rot13(char*);
+void print_number(int n);
