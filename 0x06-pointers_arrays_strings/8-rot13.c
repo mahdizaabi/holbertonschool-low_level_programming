0aea7e4124dd550586d75ed462fa8a8d3b935fd7
mahdizaabi <mahdi.zaabi@gmail.com>
2020-02-21 22:39:54 +0000

    addingfile

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * *rot13 - encoding a string to integer.
+ * @s: pointer.
+ * Return: z.
+ */
+char *rot13(char *s)
+{
+	int i = 0, j = 0;
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char t[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	while (s[i] != '\0')
+	{
+		for (j = 0; j <= 51; j++)
+	{
+			if (s[i] == a[j])
+	{
+				s[i] = t[j];
+					break;
+	}
+	}
+		i++;
+	}
+	return (s);
+	}
