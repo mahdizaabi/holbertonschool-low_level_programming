ad97f8888484b71b101f6f2c0de320a7c4c575fd
mahdizaabi <mahdi.zaabi@gmail.com>
2020-02-25 22:13:38 +0000

    addingfile

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,9 +1,9 @@
-#include <stdio.h>
+#include "holberton.h"
 
 /**
  *_strpbrk - searches a string for any of a set of bytes
  *@s: input
- *@accept: input
+ *@accept: string
  *Return: string
  */
 char *_strpbrk(char *s, char *accept)
