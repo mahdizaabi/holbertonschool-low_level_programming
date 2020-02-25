7ff95db75de73784d2ff5014299aeb1df859dfbc
mahdizaabi <mahdi.zaabi@gmail.com>
2020-02-25 20:42:25 +0000

    addingfile

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -2,3 +2,5 @@ char *_memset(char *s, char b, unsigned int n);
 char *_strchr(char *s, char c);
 char *_memcpy(char *dest, char *src, unsigned int n);
 unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
