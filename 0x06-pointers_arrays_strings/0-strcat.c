8fc839dc91f912ee6acae49e1e24b95873ee7333
mahdizaabi <mahdi.zaabi@gmail.com>
2020-02-29 08:54:37 +0000

    addingfile

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "holberton.h"
+
 /**
  * *_strcat - concatenates two strings.
  * @dest : first string.
@@ -10,17 +11,18 @@
 char *_strcat(char *dest, char *src)
 {
 int i = 0, j = 0;
-while (dest[i] != '\0')
-{
-	i++;
-}
 
-while (src[j] != '\0')
-{
-	dest[i] = src[j];
-	j++;
-	i++;
-}
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
 
 	return (dest);
 }
