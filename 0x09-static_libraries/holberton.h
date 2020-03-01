709b12c0dd2b761cdd5968d01001e4dac4e14050
mahdizaabi <mahdi.zaabi@gmail.com>
2020-03-01 15:44:21 +0000

    addingfile

---
+++ 0x09-static_libraries/holberton.h
@@ -0,0 +1,25 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+int _putchar(char);
+int _islower(int);
+int _isalpha(int);
+int _abs(int);
+int _isupper(int);
+int _isdigit(int);
+int _strlen(char *);
+void _puts(char *);
+char *_strcpy(char *, char *);
+int _atoi(char *);
+char *_strcat(char *, char *);
+char *_strncat(char *, char *, int);
+char *_strncpy(char *, char *, int);
+int _strcmp(char *, char *);
+char *_memset(char *, char, unsigned int);
+char *_memcpy(char *, char *, unsigned int);
+char *_strchr(char *, char);
+unsigned int _strspn(char *, char *);
+char *_strpbrk(char *, char *);
+char *_strstr(char *, char *);
+
+#endif
