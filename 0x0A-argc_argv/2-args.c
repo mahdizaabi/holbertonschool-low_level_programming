fc8feabba6aad1ed4ea261f9e0ec0110b3a46ab4
mahdizaabi <mahdi.zaabi@gmail.com>
2020-03-01 18:23:00 +0000

    addingfile

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -4,12 +4,11 @@
 /**
  * main - a program that prints all arguments it receives.
  * @argc: size of argv .
- * @argv: array containing the program line arguments .
+ * @argv: a pointer to the array containing the program line arguments .
  * Return: 0 .
  */
 int main(int argc, char *argv[])
 {
-	(void) argc;
 	while (*argv)
 	{
 		printf("%s\n", *argv);
