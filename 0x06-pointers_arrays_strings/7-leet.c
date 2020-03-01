e6439b20e1a7e768305d129e30da6871d3805d0e
mahdizaabi <mahdi.zaabi@gmail.com>
2020-03-01 13:56:23 +0000

    Updating code using pointers

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -3,6 +3,9 @@
  *leet - a function that encodes a string into 1337.
  *@s: string to be crypted .
  *Return: pointer to s .
+ *Update V 1.0 : In this new code , i changed 2 arrays with 2 pointers, that
+ *are pointed to the BASS adresses of the 2 arrays.
+ *in addition to that , i assigned a pointer p to s.
  */
 
 char *leet(char *s)
