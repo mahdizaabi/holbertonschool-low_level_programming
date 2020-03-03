ae83da3dcea1b440549229d5dad2ad53a6d3adf0
mahdizaabi <mahdi.zaabi@gmail.com>
2020-03-03 16:18:44 +0000

    addingfile

---
+++ 0x0B-malloc_free/holberton.h
@@ -0,0 +1,13 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+int _putchar(char);
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
+char *argstostr(int ac, char **av);
+char **strtow(char *str);
+
+#endif /* _HOLBERTON_H_ */
