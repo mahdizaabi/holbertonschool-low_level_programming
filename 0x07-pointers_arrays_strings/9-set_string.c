997dbf2deb6ff40befe1122d07f3d29246b97269
mahdizaabi <mahdi.zaabi@gmail.com>
2020-03-05 15:18:56 +0000

    addingfile

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * set_string - sets the value of a pointer to a char
+ * @s: double pointer
+ * @to: pointer
+ *
+ * Return : Void function .
+ */
+void set_string(char **s, char *to)
+{
+	 *s = to;
+}
