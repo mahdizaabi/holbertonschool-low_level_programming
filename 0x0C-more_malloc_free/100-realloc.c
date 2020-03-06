7800d324cf86cf5604b93b59d9ccd8db65d94bc9
mahdizaabi <mahdi.zaabi@gmail.com>
2020-03-06 00:45:03 +0000

    addingfile

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,44 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * _realloc - a function that reallocates a memory block.
+ * @ptr : void pointer to the allocated memory area .
+ * @old_size : the old size of the allocated memory.
+ * @new_size : the new size of reallocated memory.
+ * Return: void pointer.
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+
+	if (new_size > old_size)
+	{
+		free(ptr);
+			ptr =(int*) malloc(new_size);
+	if (ptr == NULL)
+	return (NULL);
+	else
+	return (ptr);
+	}
+
+	if (ptr == NULL)
+	{
+		return(ptr =(int*) malloc(new_size));
+
+			if  (ptr == NULL)
+				return (NULL);
+
+	}
+
+	if (new_size == old_size)
+	return (ptr);
+
+	if (new_size == 0 && ptr)
+	{
+		free(ptr);
+			return (NULL);
+	}
+
+	return (ptr);
+}
