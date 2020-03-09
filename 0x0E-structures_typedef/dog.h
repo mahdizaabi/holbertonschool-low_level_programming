48440a57342d2f5da65981d7991d34485cd0e5f3
mahdizaabi <mahdi.zaabi@gmail.com>
2020-03-09 21:58:01 +0000

    updating_header_file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -22,5 +22,6 @@ typedef struct dog dog_t;
 int _putchar(char);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+void free_dog(dog_t *d);
 
 #endif
