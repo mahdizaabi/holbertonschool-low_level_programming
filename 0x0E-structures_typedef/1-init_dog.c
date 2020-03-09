c253c27c6d39b4eae09b9e0f425d1a64fa6701b5
mahdizaabi <mahdi.zaabi@gmail.com>
2020-03-09 19:20:40 +0000

    fixing_code

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -11,10 +11,10 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d)
+	if (d != 0)
 	{
-		d->name = name;
-		d->age = age;
+		(*d).name = name;
+		(*d).age = age;
 		d->owner = owner;
 	}
 }
