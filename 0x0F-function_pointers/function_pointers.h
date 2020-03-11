5eb64be6733975fa265ad53efd887d79d60c7914
mahdizaabi <mahdi.zaabi@gmail.com>
2020-03-11 22:50:14 +0000

    adding_header

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -4,5 +4,6 @@
 int _putchar(char);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
 
 #endif
