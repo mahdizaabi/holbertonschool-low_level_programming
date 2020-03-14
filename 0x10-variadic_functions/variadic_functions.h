a22c45dc6ff06b2e7f4b95b8bee70d9314c3512a
mahdizaabi <mahdi.zaabi@gmail.com>
2020-03-14 20:22:31 +0000

    adding_file

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,9 @@
+#ifndef _VAR_H_
+#define _VAR_H_
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif
