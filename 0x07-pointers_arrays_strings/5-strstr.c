cf933a47563ad5053a2d8d3016ae7fa18740f5ce
mahdizaabi <mahdi.zaabi@gmail.com>
2020-03-20 04:05:07 +0000

    adding_file

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -7,16 +7,18 @@
 */
 char *_strstr(char *haystack, char *needle)
 {
+
 	while (*haystack)
 	{
 	char *src = haystack;
 	char *sub = needle;
+
 	while (*haystack && *sub && *haystack == *sub)
 	{
 		haystack++;
 		sub++;
 	}
-			if (!*sub)
+	if (!*sub)
 	return (src);
 	haystack = src + 1;
 	}
