43c39d4f8422cb28004ca3569e1d0e4382fc9510
mahdizaabi <mahdi.zaabi@gmail.com>
2020-03-20 04:08:58 +0000

    updating_file

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * print_chessboard - prints chessboard
+ * @a: double pointer
+ * Return: 0 Always
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar('\n');
+	}
+}
