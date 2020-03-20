21f9ece3831ffb84ce0387015fa98bf13e01aeb6
mahdizaabi <mahdi.zaabi@gmail.com>
2020-03-20 17:27:58 +0000

    adding_new_file

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -10,17 +10,17 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int i, j, p, l = 0, r = 0;
+	int i, j, p, m = 0, a = 0;
 
+	/*access to the elements through the pointer Adress */
 	for (i = 0; i < size; i++)
 	{
-		p = (i * size) + i;
-		l += *(a + p);
+		m += *(a + (i * size) + i);
 	}
 	for (j = 0; j < size; j++)
 	{
-		p = (j * size) + (size - 1 - j);
-		r += *(a + p);
+	/* setting the parameters of the pointer adresse memory */
+		a = *(a + (j * size) + (size - 1 - j));
 	}
 	printf("%i, %i\n", l, r);
 }
