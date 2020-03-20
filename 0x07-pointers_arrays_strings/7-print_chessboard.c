e68cff43efe1fc21e5b608320af509b4b8b4fb12
mahdizaabi <mahdi.zaabi@gmail.com>
2020-03-20 04:15:38 +0000

    updating_file

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -3,18 +3,16 @@
 /**
  * print_chessboard - prints chessboard
  * @a: double pointer
- * Return: 0 Always
+ * Return: 0
  */
 void print_chessboard(char (*a)[8])
 {
-	int i, j;
+	unsigned int i, j;
 
 	for (i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
-		{
 			_putchar(a[i][j]);
-		}
-		_putchar('\n');
+			_putchar('\n');
 	}
 }
