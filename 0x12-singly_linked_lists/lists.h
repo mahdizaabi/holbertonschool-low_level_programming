5fa23f6b083749c8cddeec622bae544add333a1e
mahdizaabi <mahdi.zaabi@gmail.com>
2020-03-24 17:55:22 +0000

    adding_file

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -23,6 +23,7 @@ typedef struct list_s
 } list_t;
 
 int _putchar(char);
+int _strlen(const char *);
 size_t print_list(const list_t *);
 size_t list_len(const list_t *);
 list_t *add_node(list_t **, const char *);
