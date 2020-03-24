425cea5cdad806c05dc5c51f8dcb512084fa2dfa
mahdizaabi <mahdi.zaabi@gmail.com>
2020-03-24 02:36:36 +0000

    adding_Header

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,33 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+int _putchar(char);
+size_t print_list(const list_t *);
+size_t list_len(const list_t *);
+list_t *add_node(list_t **, const char *);
+list_t *add_node_end(list_t **, const char *);
+void free_list(list_t *);
+
+
+#endif
