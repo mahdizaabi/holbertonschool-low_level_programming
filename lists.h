9cf8873ddebc18ba4a2148e8cadcde7cd81f44cb
mahdizaabi <mahdi.zaabi@gmail.com>
2020-03-24 19:08:03 +0000

    adding_file

---
+++ lists.h
@@ -0,0 +1,28 @@
+#ifndef _LNKDLISTS_H_
+#define _LNKDLISTS_H_
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+size_t print_listint(const listint_t *h);
+size_t listint_len(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
+listint_t *add_nodeint_end(listint_t **head, const int n);
+void free_listint(listint_t *head);
+void free_listint2(listint_t **head);
+int pop_listint(listint_t **head);
+
+#endif
