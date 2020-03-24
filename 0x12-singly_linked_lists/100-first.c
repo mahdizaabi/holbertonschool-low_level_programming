af919a1c3d2674eb40bd8c60e3679b4a4710542a
mahdizaabi <mahdi.zaabi@gmail.com>
2020-03-24 18:50:41 +0000

    adding_file

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void first(void) __attribute__ ((constructor));
+
+/**
+ * first - print a test before main 
+ *
+ * Return: void
+ */
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
