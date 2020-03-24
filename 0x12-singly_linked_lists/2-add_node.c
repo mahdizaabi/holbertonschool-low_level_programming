35276c5c62f8ccb0009e258d4f4bacf1c47d7b52
mahdizaabi <mahdi.zaabi@gmail.com>
2020-03-24 04:17:09 +0000

    adding_file

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strlen - finds the length of a string
+ * @str: string to be searched . 
+ *
+ * Return: length of the string
+ */
+unsigned int _strlen(char *str)
+{
+	unsigned int i;
+
+	for (i = 0; str[i]; i++)
+		;
+	return (i);
+}
+/**
+ * add_node - adds a node to the beginning of a linked list
+ * @head: double pointer to a lined list
+ * @str: string to add to the new node
+ *
+ * Return: pointer to the new node
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *newnode;
+
+	if (str == NULL)
+		return (NULL);
+
+	newnode =(list_t *)malloc(sizeof(list_t));
+	if (newnode == NULL)
+		return (NULL);
+
+	newnode->str = strdup(str);
+	newnode->len = _strlen(newnode->str);
+	newnode->next = *head;
+	*head = newnode;
+	return (newnode);
+}
