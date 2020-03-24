7c06254dcd7c975c2dc2bdb53408386dcf3770e9
mahdizaabi <mahdi.zaabi@gmail.com>
2020-03-24 02:37:09 +0000

    adding_file

---
+++ 0x12-singly_linked_lists/0-print_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_listint - print all the elements of list
+ * @h: pointer pointed to the head of the list
+ *
+ * Return: the number of the elements
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t length = 0;
+
+	while (h != 0)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		length++;
+		h = h->next;
+	}
+	return (length);
+}
