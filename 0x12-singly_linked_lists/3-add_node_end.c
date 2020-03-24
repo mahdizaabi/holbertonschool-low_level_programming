b6c7f5b80ff07bed4d3aa989d35e4357150ae802
mahdizaabi <mahdi.zaabi@gmail.com>
2020-03-24 17:55:49 +0000

    adding_file

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -30,7 +30,7 @@ list_t *add_node_end(list_t **head, const char *str)
 
 	tmp = *head;
 	/* nod memeorty allocation */
-	new_node = (list_t *)malloc(sizeof(list_t));
+	new_node = malloc(sizeof(list_t));
 
 	if (new_node == NULL)
 		return (NULL);
