03444ca3246b640d05c807d08ac5b94d2e87eaa2
mahdizaabi <mahdi.zaabi@gmail.com>
2020-03-31 22:17:34 +0000

    adding_file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -29,17 +29,19 @@ size_t _strlen(char *str)
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
-	ssize_t len;
+	int len;
 
 	if (filename == NULL)
 		return (-1);
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
-	if (text_content != NULL)
-		len = write(fd, text_content, _strlen(text_content));
-	close(fd);
+	if (text_content == NULL)
+		return (1);
+	len = write(fd, text_content, _strlen(text_content));
+
 	if (len == -1)
 		return (-1);
+	close(fd);
 	return (1);
 }
