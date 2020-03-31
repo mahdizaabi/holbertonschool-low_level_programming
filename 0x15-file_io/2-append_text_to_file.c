38e5c2732eb4c81da74281bb235d01cb7732b822
mahdizaabi <mahdi.zaabi@gmail.com>
2020-03-31 22:22:13 +0000

    addingfile

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,45 @@
+#include "holberton.h"
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+/**
+ * _strlen - finds the length of a string
+ * @str: pointer to the string
+ *
+ * Return: length of the string
+ */
+size_t _strlen(char *str)
+{
+	size_t len;
+
+	for (i = 0; str[len]; i++)
+		;
+	return (len);
+}
+
+/**
+ * append_text_to_file - appends a text at the end of a file.
+ * @filename: name of the file .
+ * @text_content: text content to be written
+ *
+ * Return: 1 on success and -1 on failure
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+	int wlen;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+	if (text_content != NULL)
+		wlen = write(fd, text_content, _strlen(text_content));
+	close(fd);
+	if (wlen == -1)
+		return (-1);
+	return (1);
+}
