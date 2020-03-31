458eb489306f177f5d0a82a75df8812c366c43f3
mahdizaabi <mahdi.zaabi@gmail.com>
2020-03-31 20:21:11 +0000

    adding_header

---
+++ 0x15-file_io/holberton.h
@@ -0,0 +1,10 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+#include <unistd.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+
+#endif
