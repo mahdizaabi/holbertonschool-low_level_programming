d2d9b5d0f9f120365eaf5cb398d826a4b8586845
mahdizaabi <mahdi.zaabi@gmail.com>
2020-03-31 20:20:41 +0000

    adding_file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,46 @@
+#include "holberton.h"
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output .
+ * @filename: name of the txt file to read .
+ * @letters: number of letters it should read and print
+ *
+ * Return: actual number of letters it could read and print
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t r, w;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buffer = malloc(letters);
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+	r = read(fd, buffer, letters);
+	close(fd);
+	if (r == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+	w = write(STDOUT_FILENO, buffer, r);
+	free(buffer);
+	if (r != w)
+		return (0);
+
+	return (w);
+}
