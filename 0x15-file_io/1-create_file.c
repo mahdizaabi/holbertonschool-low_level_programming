0f9f08b71d7e29fe9401c127dc819279974bde06
mahdizaabi <mahdi.zaabi@gmail.com>
2020-04-01 03:40:44 +0000

    adding_file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -35,12 +35,12 @@ int create_file(const char *filename, char *text_content)
 	int fd;
 	ssize_t lenw = 0;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
-	fd = open(filename, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
-	if (fd == -1)
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd < 0)
 		return (-1);
-	if (text_content != NULL)
+	if (text_content)
 		lenw = write(fd, text_content, _strlen(text_content));
 	close(fd);
 	if (lenw == -1)
