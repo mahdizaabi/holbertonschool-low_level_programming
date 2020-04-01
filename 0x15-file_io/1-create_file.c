b4747db2e55be65a4eb099d685fcd6cd78be77a2
mahdizaabi <mahdi.zaabi@gmail.com>
2020-04-01 03:45:54 +0000

    adding_file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -42,8 +42,8 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content)
 		lenw = write(fd, text_content, _strlen(text_content));
-	close(fd);
 	if (lenw == -1)
 		return (-1);
+	close(fd);
 	return (1);
 }
