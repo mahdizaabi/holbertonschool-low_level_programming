a698bc3cb1722f78cb10c0d8ea12aa7de41f3594
mahdizaabi <mahdi.zaabi@gmail.com>
2020-04-02 03:15:33 +0000

    adding_file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -21,7 +21,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (filename == NULL)
 		return (0);
-	fd = open(filename, O_RDONLY);
+	fd = open(filename0, O_RDWR);
 	if (fd == -1)
 		return (0);
 	buffer = malloc(letters);
