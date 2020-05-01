db07ad96c620cceeec4da779239b06f4979468ae
mahdizaabi <mahdi.zaabi@gmail.com>
2020-05-01 17:04:16 +0000

    adding_file

--- 0x16-doubly_linked_lists/0-print_dlistint.c
+++ 0x16-doubly_linked_lists/0-print_dlistint.c
@@ -9,12 +9,13 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t i;
+	size_t i = 0;
 
-	for (i = 0; h != NULL; i++)
+	while(h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
+		i++;
 	}
 	return (i);
 }
