043b8bf4541acaf98a406746e62af06fd62e2b72
mahdizaabi <mahdi.zaabi@gmail.com>
2020-05-02 05:38:36 +0000

    adding_file

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * add_dnodeint_end - add nod at the end of the list
+ * @head: pointer to the first nod of zee list
+ * @n: data to be stored in ze list
+ * Return: pointer to the new last node
+**/
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlisint_t *new, *fastandfurious;
+
+	if (head == NULL)
+		return (NULL);
+	new = (dlistint_t *) malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next == NULL;
+	if (*head == NULL)
+	{
+		new->prev = NULL;
+		*head = new;
+		return (new);
+	}
+	fastandfurious = *head;
+	while (fastandfurious->new)
+		fastandfurious = fastandfurious->next;
+	fastandfurious->next = new;
+	new->prev = fastandfurious;
+	return (new);
+}
