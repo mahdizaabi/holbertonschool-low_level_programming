2ed74f0dacae10ecc93f6a27da38c658088ddcdb
mahdizaabi <mahdi.zaabi@gmail.com>
2020-05-02 06:24:12 +0000

    adding_file

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -10,10 +10,10 @@ void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *the_punisher;
 
-	while(head)
+	while (head)
 	{
 		the_punisher = head->next;
-		free (head);
+		free(head);
 		head = the_punisher;
 	}
 }
