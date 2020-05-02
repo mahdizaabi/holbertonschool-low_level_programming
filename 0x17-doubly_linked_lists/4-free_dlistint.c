616ebbfdd94376686b53ba79594f2e584a56b379
mahdizaabi <mahdi.zaabi@gmail.com>
2020-05-02 06:20:14 +0000

    adding_file

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * free_dlistint - frees a linked list
+ * @head: a pointer to the linked list
+ *
+ * Return: void
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *the_punisher;
+
+	while(head)
+	{
+		the_punisher = head->next;
+		free (head);
+		head = the_punisher;
+	}
+}
