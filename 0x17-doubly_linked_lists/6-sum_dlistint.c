4d6c24b1daf06a4f121d28ca847f2438fb891ef9
mahdizaabi <mahdi.zaabi@gmail.com>
2020-05-02 07:38:30 +0000

    adding_file

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * sum_dlistint - add nod at the end of the list
+ * @head: pointer to the first nod of zee list
+ * Return: the sum of all data
+**/
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+	dlistint_t *spiderman;
+
+	spiderman = head;
+	while (spiderman->next)
+	{
+		spiderman = spiderman->next;
+		sum = sum + spiderman->n;
+	}
+
+	return (sum);
+}
+
