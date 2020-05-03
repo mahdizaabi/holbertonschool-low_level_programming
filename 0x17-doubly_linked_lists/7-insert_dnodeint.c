a170a0d147cc4658e8bb7a2d1db6fc19273e5313
Username <mahdi.zaabi@gmail.com>
2020-05-03 06:08:53 +0000

    adding_file

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -29,7 +29,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		j++;
 	}
 	if (idx == j)
-		add_dnodeint_end(h, n);
+		return (add_dnodeint_end(h, n));
 
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
