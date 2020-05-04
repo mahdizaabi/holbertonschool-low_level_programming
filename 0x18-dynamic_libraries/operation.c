e6d2d0494f7d42dc097a43d648c4af9704ea8133
Username <mahdi.zaabi@gmail.com>
2020-05-04 15:56:59 +0000

    adding_file

---
+++ 0x18-dynamic_libraries/operation.c
@@ -0,0 +1,20 @@
+int add(a, b)
+{
+	return (a + b);
+}
+int sub(a, b)
+{
+	return (a - b);
+}
+int mul(a, b)
+{
+	return (a * b);
+}
+int div(a, b)
+{
+	return (a / b);
+}
+int mod(a, b)
+{
+	return (a % b);
+}
