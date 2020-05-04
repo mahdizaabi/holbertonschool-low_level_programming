a1325032d7576e19658d0ec90ab9296f6ddd2cf3
Username <mahdi.zaabi@gmail.com>
2020-05-04 01:18:25 +0000

    adding_file

--- variadic_functions.h
+++
@@ -1,9 +0,0 @@
-#ifndef _VAR_H_
-#define _VAR_H_
-int _putchar(char c);
-int sum_them_all(const unsigned int n, ...);
-void print_numbers(const char *separator, const unsigned int n, ...);
-void print_strings(const char *separator, const unsigned int n, ...);
-void print_all(const char * const format, ...);
-
-#endif
