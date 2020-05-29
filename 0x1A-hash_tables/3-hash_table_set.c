6c8ae34194463a5681e97f034a9bfddd431b0f2b
mahdizaabi <mahdi.zaabi@gmail.com>
2020-05-29 14:19:55 +0000

    adding_file

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -79,7 +79,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			ht->array[index] = ptonode;
 		}
 		else
-			ptonode->next = NULL;
 			ht->array[index] = ptonode;
 	return (1);
 }
