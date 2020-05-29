248d8b9aaad37676e54d93e4b19e64663bf9aa4f
mahdizaabi <mahdi.zaabi@gmail.com>
2020-05-29 02:35:17 +0000

    adding_file

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -15,7 +15,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	ptotable->size = size;
-	ptotable->array = malloc(sizeof(hash_node_t*) * size);
+	ptotable->array = malloc(sizeof(hash_node_t *) * size);
 
 	while (i < ptotable->size)
 	{
