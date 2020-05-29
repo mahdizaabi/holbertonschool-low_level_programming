073b49a9d7b9c175503b3c9bc53a08340718dd81
mahdizaabi <mahdi.zaabi@gmail.com>
2020-05-29 17:27:33 +0000

    adding_file

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -43,4 +43,5 @@ char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
 hash_node_t *node_insert(const char *key, const char *value);
+char *hash_table_get(const hash_table_t *ht, const char *key);
 #endif
