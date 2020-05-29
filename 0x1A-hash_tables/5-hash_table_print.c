f0d384a36c0500565399d762fe88fa89702ff92e
mahdizaabi <mahdi.zaabi@gmail.com>
2020-05-29 18:24:41 +0000

    adding_file

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints the while h table
+ * @ht: pointer to the hashtable
+ *
+ * Return: void
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int index;
+	hash_node_t *temp;
+	int x = 0;
+
+	if (ht == NULL || ht->array == NULL)
+		return;
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		temp = ht->array[index];
+		while (temp != NULL)
+		{
+			if (x == 1)
+				printf(", ");
+			printf("'%s': '%s'", temp->key, temp->value);
+			x = 1;
+			tmp = tmp->next;
+		}
+	}
+	printf("}\n");
+}
