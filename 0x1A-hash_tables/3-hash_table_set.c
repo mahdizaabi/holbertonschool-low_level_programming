fbedcfc9f8fe1a25ec1a3e7ab1d845febd2fa6ef
mahdizaabi <mahdi.zaabi@gmail.com>
2020-05-29 13:11:52 +0000

    adding_file

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -43,7 +43,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *temp;
 
 
-	if ((key == NULL) || (ht == NULL) || ht->array == NULL)
+	if ((key == NULL) || (ht == NULL))
 	{
 		return (0);
 	}
