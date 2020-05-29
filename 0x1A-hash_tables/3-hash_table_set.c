45e00b55389b23bb73575940081f9359b14310f7
mahdizaabi <mahdi.zaabi@gmail.com>
2020-05-29 16:04:32 +0000

    adding_file

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -50,7 +50,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	char *dups;
 
 
-	if (ht == NULL || key == NULL|| value == NULL)
+	if (ht == NULL || key == NULL || value == NULL )
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
