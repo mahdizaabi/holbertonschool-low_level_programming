6ef4cec43b9c1f22204eea7ff1069ee3ec434e90
mahdizaabi <mahdi.zaabi@gmail.com>
2020-05-29 12:50:45 +0000

    adding_file

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -56,6 +56,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			{
 				free(temp->value);
 				temp->value = strdup(value);
+				if (temp->value == NULL)
+				{
+					return (0);
+				}
 				return (1);
 			}
 			temp = temp->next;
