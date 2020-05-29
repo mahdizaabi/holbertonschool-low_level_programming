a7448aec23e106ab8ee9e43e7beb34afa8f431b0
mahdizaabi <mahdi.zaabi@gmail.com>
2020-05-29 02:29:33 +0000

    adding_file

---
+++ 0x1A-hash_tables/hash_tables.h
@@ -0,0 +1,41 @@
+#ifndef _HASH_TABLE_H_
+#define _HASH_TABLE_H_
+
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+/**
+ * struct hash_node_s - Node of a hash table
+ *
+ * @key: The key, string
+ * The key is unique in the HashTable
+ * @value: The value corresponding to a key
+ * @next: A pointer to the next node of the List
+ */
+typedef struct hash_node_s
+{
+	char *key;
+	char *value;
+	struct hash_node_s *next;
+} hash_node_t;
+
+/**
+ * struct hash_table_s - Hash table data structure
+ *
+ * @size: The size of the array
+ * @array: An array of size @size
+ * Each cell of this array is a pointer to the first node of a linked list,
+ * because we want our HashTable to use a Chaining collision handling
+ */
+typedef struct hash_table_s
+{
+	unsigned long int size;
+	hash_node_t **array;
+} hash_table_t;
+
+
+hash_table_t *hash_table_create(unsigned long int size);
+unsigned long int hash_djb2(const unsigned char *str);
+
+#endif
