55ac1f145b0b93f0eed03513f4c06bd969c27b90
mahdizaabi <mahdi.zaabi@gmail.com>
2020-07-03 17:17:34 +0000

    adding_file

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def island_perimeter(grid):
+    """returns the perimeter of the island described in grid"""
+    c = 0
+    total = 0
+    for x in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[x][j] == 1:
+                if grid[x][j-1] + grid[x][j+1] + grid[x-1][j] + grid[x+1][j] == 1:
+                    c = 3
+                    total += c
+                else:
+                    total += 2
+    return(total)
+
