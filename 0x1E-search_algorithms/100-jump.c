87b524b3d62c06ea75771f5cab510f2d178b9fa0
crxs10 <59511107+mahdizaabi@users.noreply.github.com>
2020-08-26 22:05:50 +0200

    Update 100-jump.c

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -3,32 +3,73 @@
 #include <stdlib.h>
 #include <math.h>
 /**
- * search - search a value in an array
- * @a: pointer to the first element of the array
- * @l: index of the left side
- * @r: index of the right side
- * @v: value to search for
+ * print - search a value in an array
+ * @array: pointer to the first element of the array
+ * @p_before: index of the left side
+ * @gap: index of the right side
+ * @step: value to search for
+ * @gap: value between one block
+ * @value: value to search for
+ * Return: index of the value or -1 if element not found
+ */
+int print(int *array, size_t p_before, size_t gap, size_t step, int value)
+{
+	size_t x;
+
+	for (x = p_before - gap; x <= step; x++)
+	{
+		printf("Valuex checked array[%lu] = [%d]\n", x, array[x]);
+		if (array[x] == value)
+			return (x);
+	}
+	return (x);
+
+}
+/**
+ * jump_search - search a value in an array
+ * @array: pointer to the first element of the array
+ * @size: index of the left side
+ * @value: index of the right side
  * Return: index of the value or -1 if element not found
  */
 int jump_search(int *array, size_t size, int value)
 {
 	size_t step = sqrt(size);
-
+	size_t x;
+	size_t p_before = 0;
+	size_t gap = sqrt(size);
+	char *check = "Value checked array[%lu] = [%d]\n";
+	char *found = "Value found between indexes [%lu] and [%lu]\n";
 
 	if (!array)
-		return(-1);
-	while(array[step] <= value || step < size)
+		return (-1);
+	printf(check, p_before, array[p_before]);
+	while (array[step] <= value && step <= size)
 	{
-		step_before = step;
-		step = step + sqrt(n);
+		if (array[step] != value)
+			printf(check, step, array[step]);
+		p_before = step;
+		step = step + sqrt(size);
+		if (step > size)
+		{
+			printf(found, p_before, step);
+			printf(check, p_before, array[p_before]);
+			return (-1);
+		}
+	}
+	if (value >= array[gap])
+		printf(found, p_before - gap, step - gap);
+	else
+	{
+		printf("Value found between indexes [0] and [%lu]\n", gap);
+		for (x = 0; x < gap; x++)
+		{
+			printf(check, x, array[x]);
+			if (array[x] == value)
+				return (x);
+		}
+	}
+	return (print(array, p_before, gap, step, value));
 
-		if (step > size - 1)
-			return(-1)
-	}
-	for(x = step_before; x < step; x++)
-	{
-		if(array[x] == value)
-			return (x);
-	}
-	return (-1)
+	return (-1);
 }
