49325aa6c76860b1e93ef713c755ed6711a72258
mahdizaabi <mahdi.zaabi@gmail.com>
2020-08-26 21:28:44 +0000

    adding_file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -9,6 +9,6 @@ int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int search(int *a, size_t l, size_t r, int v);
 int jump_search(int *array, size_t size, int value);
-int print(int *array, size_t p_before, size_t gap, size_t step, int value);
+int print(int *array, size_t p_before, size_t step, int value);
 #endif
 
