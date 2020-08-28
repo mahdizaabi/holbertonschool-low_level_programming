9d83fcd52c7e27266dbcca9c769c43fc26e18b6e
crxs10 <59511107+mahdizaabi@users.noreply.github.com>
2020-08-28 07:47:27 +0200

    Update search_algos.h

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -9,8 +9,7 @@ int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int search(int *a, size_t l, size_t r, int v);
 int jump_search(int *array, size_t size, int value);
-int print(int *array, size_t p_before, size_t gap, size_t step, int value);
 int interpolation_search(int *array, size_t size, int value);
-
+int print(int *array, size_t p_before, size_t step, int value);
 #endif
 
